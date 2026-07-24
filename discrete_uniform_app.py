857e420f77059d6a462ae686fffa5fc1adb20c4e
Dr Nisha Arora <dr.aroranisha@gmail.com>
2026-07-24 10:39:16 +0530

    Update discrete_uniform_app.py

--- discrete_uniform_app.py
+++ discrete_uniform_app.py
@@ -128,5 +128,5 @@ st.caption(
     "Built by Dr. Nisha Arora, Analytics, Data Science & AI trainer. "
     "Found an issue, or interested in corporate training / speaking? "
     "Reach out at [LinkedIn](https://www.linkedin.com/in/drnishaarora/) or [Email](mailto:dr.aroranisha@gmail.com?subject=Hello%20Nisha&body=I%20saw%20your%20Streamlit%20app...)"
-)."
+)
 
